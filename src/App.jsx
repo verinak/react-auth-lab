bbd00f5445545fc4c3cd96f66538c1bafd308e59
verinak <verinamichelk@gmail.com>
2026-04-11 02:02:00 +0200

    gh pages routing fix

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     return (
         <>
             <AuthProvider>
-                <HashRouter basename="/react-auth-lab">
+                <HashRouter>
                     <Routes>
                         {/*  protected route */}
                         {/* <Route path="/" element={<Home />} /> */}
